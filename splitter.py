d0f1fed68e974a618e8cc6fa89f8a39b8a6dbc8d
Auctoris <ajp@auctoris.co.uk>
2018-02-18 17:17:31 +0000

    Full version - works for n>2
    This version doesn't work for n=2 (or less?) - but that falls outside
    of the typical use-cases... To be fixed later.

--- splitter.py
+++ splitter.py
@@ -1,38 +1,64 @@
 # splitter - break a long string into shorter lines - with a max length of n...
 
+
 class splitter():
 	def __init__(self, n):
 		self.n = n
 		self.lines = []
-		
+
 	def __str__(self):
 		r = ""
 		for _ in self.lines:
 			r += _ + "\n"
 		return r
-		
+
 	def split(self, string):
-		l = 0
-		for i in range (int(len(string)/self.n)+2):
-			substring = string[l:i * self.n]
-			p = substring.rfind(" ")
-			if p == -1:
-				self.lines.append(substring)
-				l = i * self.n		
+		p = 0
+		done = False
+		while not done:
+			if len(string[p:]) <= self.n:
+				self.lines.append(string[p:])
+				done = True
 			else:
-				self.lines.append(substring[:p])
-				l = l + p + 1
-						
-						
+				substring = string[p:p + self.n]
+				if substring[-1] == ' ':
+					self.lines.append(substring)
+					p += self.n
+				else:
+					q = substring.rfind(' ')
+					if q != -1:
+						self.lines.append(substring[:q])
+						p += q + 1
+					else:
+						self.lines.append(substring)
+						p += self.n
+
+
 s = splitter(20)
-s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+s.split("ABCDEFGHIJKLMNOPQRS TUVWXYZ")
 print(s)
 
-s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
-				"abcdefghijklmnopqrstuvwxyz")
+s = splitter(20)
+s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz")
 print(s)
 
+s = splitter(20)
 # The cat sat on the 
 # mat by the door
+s.split("The cat sat on the mat by the door")
+print(s)
+
+s = splitter(12)
+# The cat sat 
+# on the mat 
+# by the door
+s.split("The cat sat on the mat by the door")
+print(s)
+
+s = splitter(30)
+# The cat sat 
+# on the mat 
+# by the door
 s.split("The cat sat on the mat by the door")
 print(s)
