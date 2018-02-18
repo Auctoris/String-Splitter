cc08a7c9e80be046cc673429da18dc582ea2f4ee
Auctoris <ajp@auctoris.co.uk>
2018-02-18 12:16:37 +0000

    Move to full class-based system

--- splitter.py
+++ splitter.py
@@ -3,30 +3,36 @@
 class splitter():
 	def __init__(self, n):
 		self.n = n
+		self.lines = []
+		
+	def __str__(self):
+		r = ""
+		for _ in self.lines:
+			r += _ + "\n"
+		return r
 		
 	def split(self, string):
-		r = ""
 		l = 0
 		for i in range (int(len(string)/self.n)+2):
 			substring = string[l:i * self.n]
 			p = substring.rfind(" ")
 			if p == -1:
-				r += substring + "\n"
+				self.lines.append(substring)
 				l = i * self.n		
 			else:
-				r += substring[:p] + "\n"
+				self.lines.append(substring[:p])
 				l = l + p + 1
-		return r
-		
+						
+						
 s = splitter(20)
-string = s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-print(string)
+s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print(s)
 
-string = s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
-								 "abcdefghijklmnopqrstuvwxyz")
-print(string)
+s.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
+				"abcdefghijklmnopqrstuvwxyz")
+print(s)
 
 # The cat sat on the 
 # mat by the door
-string = s.split("The cat sat on the mat by the door")
-print(string)
+s.split("The cat sat on the mat by the door")
+print(s)
